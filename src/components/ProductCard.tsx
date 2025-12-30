db83e2e86c92251629ed7fe4abeec257bb4fb107
Mohamed Gabal <abogabal672@gmail.com>
2025-12-30 02:01:38 +0200

    finished cetegories then somethink from products

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { productType } from '@/types';
+
+const ProductCard = ({product}: {product:productType}) => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default ProductCard;
